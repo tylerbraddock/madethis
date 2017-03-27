5848ebe5134f311b2167440a7841196d56bccee9
Tyler Braddock <tyler.braddock@yahoo.com>
2017-03-27 22:15:07 +0000

    In spec/grams_controller_spec.rb, within current describe grams#create action do, added test for addressing validation errors. In model gram.rb, added validation for :message. In grams_controller.rb, adjusted create action to account for validation errors.

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -21,7 +21,13 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to redirect_to root_path
 
       gram = Gram.last
-      expect(gram.message).to eq("Hello!")      
+      expect(gram.message).to eq("Hello!")
+    end
+
+    it "should properly address validation errors" do
+      post :create, params: { gram: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(Gram.count).to eq 0
     end
   end
 end
